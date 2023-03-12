981edca9f928515279e0919c471f1af37807e93a
Rohit Chauhan <rohitchauhan404@outlook..com>
2023-03-13 02:48:44 +0530

    13 refactoring done vite working

---
+++ components/GroceryItems.js
@@ -0,0 +1,27 @@
+import { ref } from './../js/vue.js'
+
+export default {
+
+  setup() {
+
+    const groceryList = ref([
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Milk' },
+      { id: 3, text: 'Bread' },
+      { id: 4, text: 'Whatever else humans are supposed to eat' }
+    ])
+
+    return {
+      groceryList
+    }
+  },
+
+  template: `
+    <h1 class="title is-3"> Grocery Items </h1>
+    <ol>
+     <li v-for="grocery in groceryList">{{grocery.text}}</li>
+    </ol>
+    `
+}
+
---
+++ components/StationeryItems.js
@@ -0,0 +1,27 @@
+import { ref } from './../js/vue.js'
+
+export default {
+
+  setup() {
+
+    const stationeryList = ref([
+      { id: 0, text: 'Pen' },
+      { id: 1, text: 'Pencil' },
+      { id: 2, text: 'Sharpener' },
+      { id: 3, text: 'Eraser' },
+      { id: 4, text: 'Whatever else humans are supposed to write with' }
+    ])
+
+    return {
+      stationeryList
+    }
+  },
+
+  template: `
+    <h1 class="title is-3"> Stationery Items </h1>
+    <ol>
+     <li v-for="stationery in stationeryList">{{stationery.text}}</li>
+    </ol>
+    `
+}
+
